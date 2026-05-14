c3b3db69dbfb5f878225a5f66555c846134bc4eb
mjimenez7309-byte <m.jimenez7309@gmail.com>
2026-05-13 23:38:47 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "stocksmart-current-20260514-0223";
+const APP_FILES = ["./","./index.html","./manifest.json","./icon.png","https://unpkg.com/@zxing/library@0.20.0/umd/index.min.js"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES).catch(() => null)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          if (event.request.method === "GET") cache.put(event.request, copy).catch(() => null);
+        });
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
