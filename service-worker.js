2b15123200544fbf372c037e82c682ab0fb189cc
mjimenez7309-byte <m.jimenez7309@gmail.com>
2026-06-14 22:46:56 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,4 @@
-// service-worker.js
-// StockSmart v5.2.7
-// Upload this file to GitHub as: service-worker.js
-
-const CACHE_NAME = "stocksmart-v5-2-7";
-
+const CACHE_NAME = "stocksmart-v5-3-1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -11,38 +6,24 @@ const FILES_TO_CACHE = [
   "./icon.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
